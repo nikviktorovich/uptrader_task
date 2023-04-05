51434cb805f4d4fa44d90f8696a5e742a877e0f7
lnfecteDru <nikitasmn98@gmail.com>
2023-04-05 23:59:38 +0300

    Added draw menu view

--- draw_menu_app/views.py
+++ draw_menu_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def draw_menu_view(request, menu_tree: str):
+    menu_tree_list = menu_tree.split('/')
+
+    # Removing unnecessary '' path node
+    if menu_tree_list and not menu_tree_list[-1]:
+        menu_tree_list = menu_tree_list[:-1]
+
+    return render(request, 'draw_menu_app/index.html', context={
+        'menu_tree': menu_tree_list,
+    })
