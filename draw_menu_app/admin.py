9a743d830e0c7dd5c43129f623fde9c297e0e9b3
lnfecteDru <nikitasmn98@gmail.com>
2023-04-05 21:28:11 +0300

    Added Menu model admin class

--- draw_menu_app/admin.py
+++ draw_menu_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+class MenuAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Menu, MenuAdmin)
